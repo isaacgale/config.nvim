89e28bd9fb723609c35a0a784069de66bcebcc09
isaacgale <138847930+isaacgale@users.noreply.github.com>
2024-12-20 14:50:02 +1100

    Added a TODO for jdtls keymaps, and added indent-blankline plugin

--- lua/config/lspkeymaps.lua
+++ lua/config/lspkeymaps.lua
@@ -42,6 +42,43 @@ function M.on_attach(client, bufnr)
     nmap("<leader>dn", jdtls.test_nearest_method, 'Debug/Run test method')
     nmap("<leader>oi", jdtls.organize_imports, '[O]rganize [I]mports')
 
+--[[
+    --TODO: Figure out what I want from this keymap list 
+
+    "command! -buffer -nargs=? -complete=custom,v:lua.require'jdtls'._complete_compile JdtCompile lua require('jdtls').compile(<f-args>)")
+    -- Allow yourself/register to run JdtUpdateConfig as a Vim command
+    vim.cmd("command! -buffer JdtUpdateConfig lua require('jdtls').update_project_config()")
+    -- Allow yourself/register to run JdtBytecode as a Vim command
+    vim.cmd("command! -buffer JdtBytecode lua require('jdtls').javap()")
+    -- Allow yourself/register to run JdtShell as a Vim command
+    vim.cmd("command! -buffer JdtJshell lua require('jdtls').jshell()")
+
+    -- Set a Vim motion to <Space> + <Shift>J + o to organize imports in normal mode
+    vim.keymap.set('n', '<leader>Jo', "<Cmd> lua require('jdtls').organize_imports()<CR>",
+        { desc = "[J]ava [O]rganize Imports" })
+    -- Set a Vim motion to <Space> + <Shift>J + v to extract the code under the cursor to a variable
+    vim.keymap.set('n', '<leader>Jv', "<Cmd> lua require('jdtls').extract_variable()<CR>",
+        { desc = "[J]ava Extract [V]ariable" })
+    -- Set a Vim motion to <Space> + <Shift>J + v to extract the code selected in visual mode to a variable
+    vim.keymap.set('v', '<leader>Jv', "<Esc><Cmd> lua require('jdtls').extract_variable(true)<CR>",
+        { desc = "[J]ava Extract [V]ariable" })
+    -- Set a Vim motion to <Space> + <Shift>J + <Shift>C to extract the code under the cursor to a static variable
+    vim.keymap.set('n', '<leader>JC', "<Cmd> lua require('jdtls').extract_constant()<CR>",
+        { desc = "[J]ava Extract [C]onstant" })
+    -- Set a Vim motion to <Space> + <Shift>J + <Shift>C to extract the code selected in visual mode to a static variable
+    vim.keymap.set('v', '<leader>JC', "<Esc><Cmd> lua require('jdtls').extract_constant(true)<CR>",
+        { desc = "[J]ava Extract [C]onstant" })
+    -- Set a Vim motion to <Space> + <Shift>J + t to run the test method currently under the cursor
+    vim.keymap.set('n', '<leader>Jt', "<Cmd> lua require('jdtls').test_nearest_method()<CR>",
+        { desc = "[J]ava [T]est Method" })
+    -- Set a Vim motion to <Space> + <Shift>J + t to run the test method that is currently selected in visual mode
+    vim.keymap.set('v', '<leader>Jt', "<Esc><Cmd> lua require('jdtls').test_nearest_method(true)<CR>",
+        { desc = "[J]ava [T]est Method" })
+    -- Set a Vim motion to <Space> + <Shift>J + <Shift>T to run an entire test suite (class)
+    vim.keymap.set('n', '<leader>JT', "<Cmd> lua require('jdtls').test_class()<CR>", { desc = "[J]ava [T]est Class" })
+    -- Set a Vim motion to <Space> + <Shift>J + u to update the project configuration
+    vim.keymap.set('n', '<leader>Ju', "<Cmd> JdtUpdateConfig<CR>", { desc = "[J]ava [U]pdate Config" })
+    ]]
   end
 
   -- Create a command `:Format` local to the LSP buffer
